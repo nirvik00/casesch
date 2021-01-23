76ad3b98da635200bb9d7f7a18209c604d4be1c0
nirvik00 <nirviksaha@gatech.edu>
2021-01-22 20:02:33 -0500

    del

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def hello():
     return "Hello, World from nirvik saha!"
 
 
+@app.route("/get", methods=["GET"])
+def get_req():
+    return "get request
+
+
 if __name__ == "__main__":
     app.run()
