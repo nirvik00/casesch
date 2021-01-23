f47fb0faf41970f6b5ae8e7a0611c78379b50245
nirvik00 <nirviksaha@gatech.edu>
2021-01-22 20:01:28 -0500

    del

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello, World from nirvik saha!"
-
-
-@app.route("/get", methods=["GET"])
-def getApi():
-    return jsonify({'x': 'get request'})
 
 
 if __name__ == "__main__":
