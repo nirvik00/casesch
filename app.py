8ee5e5ca2441ecb6d3a29a3331d1d8186d56eb7d
nirvik00 <nirviksaha@gatech.edu>
2021-01-24 04:18:51 -0500

    updated gen val

--- app.py
+++ app.py
@@ -2,32 +2,12 @@ import os
 import sys
 import csv
 import io
-
+import functools
 from flask import Flask, jsonify, render_template, request, make_response
-
 from werkzeug.utils import secure_filename
-
 from tools.validate_sch import validate_schematron, print_failure
 from tools.generate_sch import generate_sch
 from tools.clean_xml import clean_files
-
-
-def validate_schematrons(test_val_sch, test_val_xml):
-    num_errors = 0
-    failures = validate_schematron(
-        test_val_sch,
-        test_val_xml,
-        result_path="val.txt",
-        strict_context=False
-    )
-    s = ""
-    for f in failures:
-        if f.role == 'ERROR':
-            num_errors += 1
-        s += print_failure(test_val_sch, f)
-        s += "\n"
-    return s
-
 
 app = Flask(__name__)
 
@@ -56,9 +36,8 @@ def file_save_generate_sch():
         with open(csv_file, encoding='utf-8-sig') as f:
             rows = [{k: v for k, v in row.items()}
                     for row in csv.DictReader(f, skipinitialspace=True)]
-        gen_res_str = generate_sch(rows)
         os.remove(csv_file)
-        return gen_res_str
+        return generate_sch(rows)
     except:
         return jsonify({"error": "server error - Check files"})
 
@@ -66,31 +45,14 @@ def file_save_generate_sch():
 @ app.route("/api/validate", methods=["POST"])
 def validate():
     try:
-        file_storage_sch = request.files['schfile']
+        file_storage_sch, file_storage_xml = request.files['schfile'], request.files['xmlfile']
         file_storage_sch.save(secure_filename(file_storage_sch.filename))
-        test_val_sch = file_storage_sch.filename
-
-        file_storage_xml = request.files['xmlfile']
         file_storage_xml.save(secure_filename(file_storage_xml.filename))
-        test_val_xml = file_storage_xml.filename
-
-        num_errors = 0
+        test_val_sch, test_val_xml = file_storage_sch.filename, file_storage_xml.filename
         failures = validate_schematron(
-            test_val_sch,
-            test_val_xml,
-            result_path="val.txt",
-            strict_context=False
-        )
-        s = ""
-        for f in failures:
-            if f.role == 'ERROR':
-                num_errors += 1
-            s += print_failure(test_val_sch, f)
-            s += "\n"
-        os.remove(test_val_sch)
-        os.remove(test_val_xml)
-        os.remove("val.txt")
-        return s
+            test_val_sch, test_val_xml, result_path="val.txt", strict_context=False)
+        map(lambda x: os.remove(x), [test_val_sch, test_val_xml, "val.txt"])
+        return functools.reduce((lambda x, y: str(x)+"\n"+str(y)), failures)
     except:
         return jsonify({"error": "server error - Check files"})
 
