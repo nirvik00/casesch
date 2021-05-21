1ccd2c69c91287d3795ae64afd3fa1d01e39ac48
nirvik00 <nsdev404@gmail.com>
2021-05-21 12:59:09 -0400

    file system

--- app.py
+++ app.py
@@ -37,21 +37,43 @@ def file_save_generate_sch():
         return jsonify({"error": "server error - Check files"})
 
 
-@ app.route("/api/validate", methods=["POST"])
-def validate():
-    try:
-        file_storage_sch, file_storage_xml = request.files['schfile'], request.files['xmlfile']
-        file_storage_sch.save(secure_filename(file_storage_sch.filename))
-        file_storage_xml.save(secure_filename(file_storage_xml.filename))
-        test_val_sch, test_val_xml = file_storage_sch.filename, file_storage_xml.filename
-        failures = validate_schematron(
-            test_val_sch, test_val_xml, result_path="val.txt", strict_context=False)
-        os.remove(test_val_sch)
-        os.remove(test_val_xml)
-        os.remove("val.txt")
-        return {"out": failures}
-    except:
-        return {"x": "error", "y": "check file encoding: should be UTF-8", "status": "500"}
+@ app.route("/api/upload", methods=["POST"])
+def upload():
+    msg=""
+    t=False
+    if request.method== "POST":
+        try:
+            file_1= request.files['file']
+            file_1.save(file_1.filename)
+            msg += "\nfile -1 saved"
+            t=True
+        except:
+            msg += "\nerror-1 in file not saved"
+        if t==True:
+            return jsonify({"msg": msg})
+        else:
+            try:
+                file_1= request.files['filename']
+                file_1.save(file_1.filename)
+                msg += "\nfile -2 saved"
+            except:
+                msg+= "\nerror-2 in file not saved"
+    return msg
+
+@ app.route("/api/validate-x", methods=["POST", "GET"])
+def validateX():
+    print("validate-x")
+    if request.method=="POST":
+        test_val_sch  = "BEQ_Mappings_medium.sch"
+        test_val_xml = "BuildingSync_sample_medium_01.xml"
+        failures = validate_schematron(test_val_sch, test_val_xml, "val.txt", strict_context=False)
+        print(failures)
+        # os.remove(file_storage_xml.filename)
+        # os.remove(file_storage_sch.filename)
+        return ({"out": failures})
+    else:
+        return ({"msg": "send post req"})
+
 
 @ app.route("/api/validate-1", methods=["POST", "GET"])
 def validate1():
@@ -66,8 +88,8 @@ def validate1():
         failures = validate_schematron(test_val_sch, test_val_xml, "val.txt", strict_context=False)
         print(failures)
 
-        #os.remove(file_storage_xml.filename)
-        #os.remove(file_storage_sch.filename)
+        os.remove(file_storage_xml.filename)
+        os.remove(file_storage_sch.filename)
 
         return ({"out": failures})
     else:
